79c5d45ff85d29bbef9ff311adc59c86395eea06
Jakub Panek <jakub.panek@educanet.cz>
2020-12-01 09:51:48 +0100

    zalozeny projekt

--- src/main/java/cz/educanet/webik/Users.java
+++ src/main/java/cz/educanet/webik/Users.java
@@ -1,13 +1,17 @@
 package cz.educanet.webik;
 
 public class Users {
+    private String FirstName;
+    private String LastName;
     private String username;
     private String email;
     private String password;
-    public Users(String username, String password, String email) {
+    public Users(String username, String password, String email, String FirstName, String LastName) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
     };
 
 
@@ -17,6 +21,14 @@ public class Users {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
     }
 
 }
